3cc644085bbd20842881bc627aef3eb224c71b1f
Shaila <144827061+shailaMoulee@users.noreply.github.com>
2024-03-07 11:08:44 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("boxAreas");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+}
+
+function openMenu() {
+  document.getElementById("menu").style.width = "100%";
+}
+
+function closeMenu() {
+  document.getElementById("menu").style.width = "0%";
+}
